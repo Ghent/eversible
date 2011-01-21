3d6b2d1cb321102f67f766d29838db986773b168
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-01-21 19:19:58 +0000

    Brushed up sov.py

--- irc/pub/sov.py
+++ irc/pub/sov.py
@@ -7,14 +7,26 @@ def index(connection,event):
     systemName = event.arguments()[0].split()[1]
     sovInfo = API.Map("sov",systemName)
     if sovInfo:
+        #update API to return system security, regionName and constellationName
         solarSystemName = sovInfo["solarSystemName"]
         allianceName = sovInfo["allianceName"]
         allianceTicker = sovInfo["allianceTicker"]
         factionName = sovInfo["factionName"]
         corporationName = sovInfo["corporationName"]
         if factionName:
-            connection.privmsg(event.target(), "System '%s' is owned by '%s'" % (solarSystemName, factionName))
+            outputMessage = """
+                \x02System\x02: %s (\x1fhttp://evemaps.dotlan.net/system/%s\x1f)
+                \x032Owner\x03:  %s (NPC)
+            """ % (solarSystemName, solarSystemName, factionName)
+            for line in outputMessage.split("\n"):
+                connection.privmsg(event.target(), line.strip())
         else:
-            connection.privmsg(event.target(), "System '%s' is owned by '%s' of alliance '%s' (%s)" % (solarSystemName, corporationName, allianceName, allianceTicker))
+            outputMessage = """
+                \x02System\x02:   %s (\x1fhttp://evemaps.dotlan.net/system/%s\x1f)
+                \x039Owner\x03:    %s
+                \x035Alliance\x03: %s (%s)
+            """ % (solarSystemName, solarSystemName, corporationName, allianceName, allianceTicker)
+            for line in outputMessage.split("\n"):
+                connection.privmsg(event.target(), line.strip())
     else:
-        connection.privmsg(event.target(), "System '%s' does not exist" % systemName)
+        connection.privmsg(event.target(), "System '\x034%s\x03' does not exist" % systemName)
