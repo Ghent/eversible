113dc7163e89e3fc5e31f1089f39e026d508bc89
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-01-22 21:33:38 +0000

    Changed formatting to be consistent with other commands

--- irc/pub/kills.py
+++ irc/pub/kills.py
@@ -37,7 +37,7 @@ def index(connection, event):
             constellationName = systemInfo["constellationName"]
             
             connection.privmsg(event.target(), "\x02System\x02:  \x02%s\x02/\x0310%s\x03/\x032%s\x03 (%s)" % (solarSystemName, constellationName, regionName, sec))
-            connection.privmsg(event.target(), "\x032Ships\x03:   %i" % shipKills)
-            connection.privmsg(event.target(), "\x037Pods\x03:    %i" % podKills)
-            connection.privmsg(event.target(), "\x034NPCs\x03:    %i" % npcKills)
+            connection.privmsg(event.target(), "\x02Ships\x02:   %i" % shipKills)
+            connection.privmsg(event.target(), "\x02Pods\x02:    %i" % podKills)
+            connection.privmsg(event.target(), "\x024NPCs\x02:    %i" % npcKills)
             #connection.privmsg(event.target(), "In the last hour, there were %i ship kills, %i pod kills and %i NPC kills in system %s" % (shipKills, podKills, npcKills, solarSystemName))
