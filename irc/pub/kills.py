dba075956121383e0b6d99eeb552774491fd8cf7
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-01-22 12:02:03 +0000

    Added security rating to output using CCP static database data

--- irc/pub/kills.py
+++ irc/pub/kills.py
@@ -4,8 +4,10 @@
 
 
 import api
+import db
 
 API = api.API()
+DB = db.DUMP()
 
 def index(connection, event):
     try:
@@ -22,8 +24,20 @@ def index(connection, event):
             podKills = response["podKills"]
             npcKills = response["npcKills"]
             solarSystemName = response["solarSystemName"]
-            connection.privmsg(event.target(), "\x02System\x02 : %s" % solarSystemName)
-            connection.privmsg(event.target(), "\x038Ships\x03  : %i" % shipKills)
+            
+            systemInfo = DB.getSystemInfoByID(response["solarSystemID"])
+            security = round(systemInfo["security"],1)
+            #note: \x02\x02 is added to prevent security digits clashing with the colour codes
+            if security >= 0.5:
+                sec = "\x033\x02\x02%.01f\x03" % security
+            elif security < 0.5 and security > 0.0:
+                sec = "\x037\x02\x02%.01f\x03" % security
+            else:
+                sec = "\x035\x02\x02%.01f\x03" % security
+            
+            
+            connection.privmsg(event.target(), "\x02System\x02 : \x02%s\x02 (%s)" % (solarSystemName, sec))
+            connection.privmsg(event.target(), "\x032Ships\x03  : %i" % shipKills)
             connection.privmsg(event.target(), "\x037Pods\x03   : %i" % podKills)
             connection.privmsg(event.target(), "\x034NPCs\x03   : %i" % npcKills)
             #connection.privmsg(event.target(), "In the last hour, there were %i ship kills, %i pod kills and %i NPC kills in system %s" % (shipKills, podKills, npcKills, solarSystemName))
