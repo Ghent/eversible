b10a21f41cea5a0a68654d18a40ca5851fc006f3
Ghent <ghentlinux@gmail.com>
2011-01-22 10:28:41 -0600

    Change doutput format to be more consistent with other modules

--- irc/pub/kills.py
+++ irc/pub/kills.py
@@ -36,8 +36,8 @@ def index(connection, event):
             regionName = systemInfo["regionName"]
             constellationName = systemInfo["constellationName"]
             
-            connection.privmsg(event.target(), "\x02System\x02 : \x02%s\x02/%s/%s (%s)" % (solarSystemName, constellationName, regionName, sec))
-            connection.privmsg(event.target(), "\x032Ships\x03  : %i" % shipKills)
-            connection.privmsg(event.target(), "\x037Pods\x03   : %i" % podKills)
-            connection.privmsg(event.target(), "\x034NPCs\x03   : %i" % npcKills)
+            connection.privmsg(event.target(), "\x02System\x02:  \x02%s\x02/\x0310%s\x03/\x032%s\x03 (%s)" % (solarSystemName, constellationName, regionName, sec))
+            connection.privmsg(event.target(), "\x032Ships\x03:   %i" % shipKills)
+            connection.privmsg(event.target(), "\x037Pods\x03:    %i" % podKills)
+            connection.privmsg(event.target(), "\x034NPCs\x03:    %i" % npcKills)
             #connection.privmsg(event.target(), "In the last hour, there were %i ship kills, %i pod kills and %i NPC kills in system %s" % (shipKills, podKills, npcKills, solarSystemName))
