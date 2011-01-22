7e0577ca8b258757cbc4e0c52ea7a299361baece
Ghent <ghentlinux@gmail.com>
2011-01-21 20:36:30 -0600

    Added functionality to read configuration from config file

--- eversible.py
+++ eversible.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python
+# vim: filetype=python tabstop=4 expandtab:
+
+import ConfigParser
+from optparse import OptionParser
 
 import irc.bot
 
+""" Get comand line arguments """
+parser = OptionParser()
+parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
+                  default=True, help="Run with output")
+parser.add_option("-q", "--quiet", action="store_false", dest="verbose",
+                  default=False, help="Run with no output")
+parser.add_option("-c", "--config", dest="configfile",
+                  default="etc/eversible.cfg", help="Config file for IRC "
+                  "bot [default: %default]")
+(options, args) = parser.parse_args()
+
+
+""" Load the configuration """
+config = ConfigParser.RawConfigParser()
+config.readfp(open(options.configfile))
+
+""" Start the IRC bot """
 irc.bot.start()
+
+quit()
