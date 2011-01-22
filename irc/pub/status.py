3757d33d8e24e44a5d48abbe9980d7ff2eb38c4d
Ghent <ghentlinux@gmail.com>
2011-01-22 05:15:46 -0600

    Changed output format to match other modules

--- irc/pub/status.py
+++ irc/pub/status.py
@@ -12,7 +12,16 @@ def index(connection, event):
     status = serverstatus["status"]
     online = serverstatus["online"]
     if status == "Online":
-        message = "Server is currently online with %i users" % online
+        message = """
+            \x02Server\x02:  \x02Tranquility\x02
+            \x02Status\x02:  \x033Online\x033
+            \x02Players\02x: \x032%i\x032
+        """ % online
     elif status == "Offline":
-        message = "Server is currently offline"
+        message = """
+            \x02Server\x02:  \x02Tranquility\x02
+            \x02Status\x02:  \x035Offline\x035
+        """
+        for line in message.split("\n"):
+            connection.privmsg(event.target(), line.strip())
     connection.privmsg(event.target(), message)
