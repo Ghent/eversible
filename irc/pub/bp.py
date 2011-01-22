6b4bdf8a3d6f195e3a35c7ef3ba4c590aca8779b
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-01-22 22:30:16 +0000

    Added some formatting

--- irc/pub/bp.py
+++ irc/pub/bp.py
@@ -31,13 +31,41 @@ def index(connection, event):
         if "Morphite" in materials.keys():
             mor = materials["Morphite"]
             
-        message = """
-            \x02Item\x02      : %s
-            \x02Materials\x02 : [Tri] %i [Pye] %i [Mex] %i [Iso] %i
-                              : [Noc] %i [Zyd] %i [Meg] %i [Mor] %i
-        """ % (itemName,
-               tri, pye, mex, iso,
-               noc, zyd, meg, mor)
+        column1_1 = " Tri=%i " % tri
+        column1_2 = " Noc=%i " % noc
         
-        for line in message.split("\n"):
-            connection.privmsg(event.target(), line.strip())
+        column2_1 = " Pye=%i " % pye
+        column2_2 = " Zyd=%i " % zyd
+        
+        column3_1 = " Mex=%i " % mex
+        column3_2 = " Meg=%i " % meg
+        
+        column4_1 = " Iso=%i " % iso
+        column4_2 = " Mor=%i " % mor
+        
+        if len(column1_1) > len(column1_2):
+            diff = len(column1_1) - len(column1_2)
+            column1_2 += " "*diff
+        else:
+            column1_1 += " "*(len(column1_2) - len(column1_1))
+            
+        if len(column2_1) > len(column2_2):
+            column2_2 += " "*(len(column2_1) - len(column2_2))
+        else:
+            column2_1 += " "*(len(column2_2) - len(column2_1))
+            
+        if len(column3_1) > len(column3_2):
+            column3_2 += " "*(len(column3_1) - len(column3_2))
+        else:
+            column3_1 += " "*(len(column3_2) - len(column3_1))
+            
+        if len(column4_1) > len(column4_2):
+            column4_2 += " "*(len(column4_1) - len(column4_2))
+        else:
+            column4_1 += " "*(len(column4_2) - len(column4_1))
+            
+        tri,pye,mex,iso,noc,zyd,meg,mor = column1_1, column2_1, column3_1, column4_1, column1_2, column2_2, column3_2, column4_2
+            
+        connection.privmsg(event.target(), "\x02Item\x02      : %s" % itemName)
+        connection.privmsg(event.target(), "\x02Materials\x02 : %s %s %s %s" % (tri, pye, mex, iso))
+        connection.privmsg(event.target(), "          : %s %s %s %s" % (noc, zyd, meg, mor))
