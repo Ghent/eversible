9bca1821d0a55ea99f4417e22a0c148e8afc95a7
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-01-22 12:42:08 +0000

    added functions:   getRegionNameByID and getConstellationNameByID modified function: getSystemInfoByID to use above additions modified syntax:   all database queries are now more human readable

--- db/__init__.py
+++ db/__init__.py
@@ -13,7 +13,10 @@ class DUMP:
         self.cursor = conn.cursor()
 
     def getItemNameByID(self, ID):
-        self.cursor.execute("SELECT typeID,typeName FROM invTypes WHERE typeID='%s'" % ID)
+        self.cursor.execute("""SELECT typeID,typeName
+                            FROM invTypes
+                            WHERE typeID='%s'
+                            """ % ID)
         row = self.cursor.fetchone()
         if row:
             return row[1]
@@ -21,7 +24,10 @@ class DUMP:
             return None
 
     def getItemIDByName(self, name):
-        self.cursor.execute("SELECT typeID,typeName FROM invTypes WHERE typeName='%s'" % name)
+        self.cursor.execute("""SELECT typeID,typeName
+                            FROM invTypes
+                            WHERE typeName='%s'
+                            """ % name)
         row = self.cursor.fetchone()
         if row:
             return row[0]
@@ -29,7 +35,11 @@ class DUMP:
             return None
 
     def getSystemNameByID(self, systemID):
-        self.cursor.execute("SELECT solarSystemID,solarSystemName FROM mapSolarSystems WHERE solarSystemID='%s'" % systemID)
+        self.cursor.execute("""
+                            SELECT solarSystemID,solarSystemName
+                            FROM mapSolarSystems
+                            WHERE solarSystemID='%s'
+                            """ % systemID)
         row = self.cursor.fetchone()
         if row:
             return row[1]
@@ -37,15 +47,46 @@ class DUMP:
             return None
 
     def getSystemIDByName(self, systemname):
-        self.cursor.execute("SELECT solarSystemID,solarSystemName FROM mapSolarSystems WHERE solarSystemName LIKE '%s'" % systemname)
+        self.cursor.execute("""
+                            SELECT solarSystemID,solarSystemName
+                            FROM mapSolarSystems
+                            WHERE solarSystemName LIKE '%s'
+                            """ % systemname)
         row = self.cursor.fetchone()
         if row:
             return row[0]
         else:
             return None
+    
+    def getConstellationNameByID(self, ID):
+        self.cursor.execute("""
+                            SELECT constellationID, constellationName
+                            FROM mapConstellations
+                            WHERE constellationID='%s'
+                            """ % ID)
+        row = self.cursor.fetchone()
+        if row:
+            return row[1]
+        else:
+            return None
+    
+    def getRegionNameByID(self, ID):
+        self.cursor.execute("""
+                            SELECT regionID, regionName
+                            FROM mapRegions
+                            WHERE regionID='%s'
+                            """ % ID)
+        row = self.cursor.fetchone()
+        if row:
+            return row[1]
+        else:
+            return None
         
     def getFactionNameByID(self, factionID):
-        self.cursor.execute("SELECT factionID,factionName FROM chrFactions WHERE factionID='%s'" % factionID)
+        self.cursor.execute("""SELECT factionID,factionName
+                            FROM chrFactions
+                            WHERE factionID='%s'
+                            """ % factionID)
         row = self.cursor.fetchone()
         if row:
             return row[1]
@@ -54,7 +95,7 @@ class DUMP:
         
     def getSystemInfoByID(self, systemID):
         self.cursor.execute("""
-                            SELECT solarSystemID,solarSystemName,security,securityClass
+                            SELECT solarSystemID,solarSystemName,security,securityClass,regionID,constellationID
                             FROM mapSolarSystems
                             WHERE solarSystemID='%s'
                             """ % systemID)
@@ -64,7 +105,11 @@ class DUMP:
                 "solarSystemID" : row[0],
                 "solarSystemName" : row[1],
                 "security" : row[2],
-                "securityClass" : row[3]
+                "securityClass" : row[3],
+                "regionID" : row[4],
+                "regionName" : self.getRegionNameByID(row[4]),
+                "constellationID" : row[5],
+                "constellationName" : self.getConstellationNameByID(row[5])
             }
         else:
             return None
