dee81df995a5b3640b8357b15fb1a761d5266ca9
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-01-22 21:34:59 +0000

    Fixed typo

--- irc/pub/kills.py
+++ irc/pub/kills.py
@@ -39,5 +39,5 @@ def index(connection, event):
             connection.privmsg(event.target(), "\x02System\x02:  \x02%s\x02/\x0310%s\x03/\x032%s\x03 (%s)" % (solarSystemName, constellationName, regionName, sec))
             connection.privmsg(event.target(), "\x02Ships\x02:   %i" % shipKills)
             connection.privmsg(event.target(), "\x02Pods\x02:    %i" % podKills)
-            connection.privmsg(event.target(), "\x024NPCs\x02:    %i" % npcKills)
+            connection.privmsg(event.target(), "\x02NPCs\x02:    %i" % npcKills)
             #connection.privmsg(event.target(), "In the last hour, there were %i ship kills, %i pod kills and %i NPC kills in system %s" % (shipKills, podKills, npcKills, solarSystemName))
