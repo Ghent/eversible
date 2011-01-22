49fbf96f31ceef4661fae10503d5734d0c29cda3
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-01-22 12:13:52 +0000

    Added security status and security class

--- irc/pub/sov.py
+++ irc/pub/sov.py
@@ -4,7 +4,9 @@
 
 
 import api
+import db
 API = api.API()
+DB = db.DUMP()
 
 def index(connection,event):
     try:
@@ -20,11 +22,24 @@ def index(connection,event):
             allianceTicker = sovInfo["allianceTicker"]
             factionName = sovInfo["factionName"]
             corporationName = sovInfo["corporationName"]
+            
+            systemInfo = DB.getSystemInfoByID(sovInfo["solarSystemID"])
+            security = systemInfo["security"]
+            if security >= 0.5:
+                sec = "\x033\x02\x02%.4f\x03" % security
+            elif security < 0.5 and security > 0.0:
+                sec = "\x037\x02\x02%.4f\x03" % security
+            else:
+                sec = "\x035\x02\x02%.4f\x03" % security
+                
+            securityClass = systemInfo["securityClass"]
+                
             if factionName:
                 outputMessage = """
-                    \x02System\x02: \x02%s\x02 :: \x1fhttp://evemaps.dotlan.net/system/%s\x1f
-                    \x032Owner\x03:  %s
-                """ % (solarSystemName, solarSystemName, factionName)
+                    \x02System\x02:  \x02%s\x02 :: \x1fhttp://evemaps.dotlan.net/system/%s\x1f
+                    \x032Owner\x03:   %s
+                    \x02Security\x02: %s (%s)
+                """ % (solarSystemName, solarSystemName, factionName, sec, securityClass)
                 for line in outputMessage.split("\n"):
                     connection.privmsg(event.target(), line.strip())
             else:
@@ -32,7 +47,8 @@ def index(connection,event):
                     \x02System\x02:   \x02%s\x02 \x02::\x02 \x1fhttp://evemaps.dotlan.net/system/%s\x1f
                     \x039Owner\x03:    %s
                     \x035Alliance\x03: %s \x02[%s]\x02
-                """ % (solarSystemName, solarSystemName, corporationName, allianceName, allianceTicker)
+                    \x02Security\x02: %s (%s)
+                """ % (solarSystemName, solarSystemName, corporationName, allianceName, allianceTicker, sec, securityClass)
                 for line in outputMessage.split("\n"):
                     connection.privmsg(event.target(), line.strip())
         else:
