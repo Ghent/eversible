cbfedf883a2fecb0770099a463d93d7474f77662
Ghent <ghentlinux@gmail.com>
2011-01-22 01:43:30 -0600

    Cleaned up error logic and code styling

--- irc/pub/route.py
+++ irc/pub/route.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python
+#
+# vim: filetype=python tabstop=4 expandtab:
+
 
 import api
+
 
 API = api.API()
 
 def index(connection, event):
     try:
         systemFrom = event.arguments()[0].split()[1]
-	systemTo = event.arguments()[0].split()[2]
+        systemTo = event.arguments()[0].split()[2]
     except IndexError:
-        connection.privmsg(event.target(), "Syntax is: .route [system name] [system name]")
+        connection.privmsg(event.target(),
+            "Syntax is: .route [system name] [system name]")
     else:
-        response = API.Map("sov", systemFrom)
-	response2 = API.Map("sov", systemTo)
-        if not response:
-            connection.privmsg(event.target(), "System '%s' is unknown to me" % systemFrom)
-	if not response2:
-	    connection.privmsg(event.target(), "System '%s' is unkonwn to me" % systemTo)
+        responseSystemFrom = API.Map("sov", systemFrom)
+        responseSystemTo = API.Map("sov", systemTo)
+        if not responseSystemFrom:
+            connection.privmsg(event.target(), "System '%s' is unknown to me"
+                % systemFrom)
+        elif not responseSystemTo:
+            connection.privmsg(event.target(), "System '%s' is unkonwn to me"
+                % systemTo)
         else:
-		systemNameFrom = response["solarSystemName"]
-		systemNameTo = response2["solarSystemName"] 
-		connection.privmsg(event.target(), "Route from %s To %s :" % (systemNameFrom, systemNameTo))
-	    	connection.privmsg(event.target(), "http://evemaps.dotlan.net/route/%s:%s" % (systemNameFrom, systemNameTo))
+            systemNameFrom = responseSystemFrom["solarSystemName"]
+            systemNameTo = responseSystemTo["solarSystemName"]
+            connection.privmsg(event.target(),
+                "Route from %s To %s :"
+                % (systemNameFrom, systemNameTo))
+            connection.privmsg(event.target(),
+                "http://evemaps.dotlan.net/route/%s:%s"
+                % (systemNameFrom, systemNameTo))
