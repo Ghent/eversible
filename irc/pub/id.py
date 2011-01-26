ae629b35c1daa89a26fc9eea87c904db71d5e8b9
petllama <petllama@gmail.com>
2011-01-26 18:24:45 +0100

    Added irc/pub/id.py for easy itemID lookup.

---
+++ irc/pub/id.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# vim: filetype=python tabstop=4 expandtab:
+
+
+import api
+import evedb
+
+
+API = api.API()
+DB = evedb.DUMP()
+
+def index(connection, event):
+    try:
+        idSearch = event.arguments()[0].split()[1]
+    except (IndexError, ValueError):
+        connection.privmsg(event.target(),
+            "Syntax is: .id [itemName]")
+    else:
+        responseItemName = DB.getItemIDByName(idSearch)
+    if not responseItemName:
+            connection.privmsg(event.target(), "Item '%s' is unknown to me"
+                % idSearch)
+    else:
+	connection.privmsg(event.target(),
+                    "\x02ID of %s\x02: \x1f%s\x1f"
+                    % (idSearch, responseItemName))
