d335bb2de83a1b4bee08579ca939cfe36989eab1
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-01-30 15:01:00 +0000

    Added partial support for getItemInfoByID -> not finished yet

--- evedb/__init__.py
+++ evedb/__init__.py
@@ -12,6 +12,125 @@ class DUMP:
         conn = sqlite3.connect("evedb/current.db")
         self.cursor = conn.cursor()
 
+    def getItemInfoByID(self, ID):
+        #invTypes
+        self.cursor.execute("""
+                            SELECT *
+                            FROM invTypes
+                            WHERE typeID='%s'
+                            """ % ID)
+        row = self.cursor.fetchone()
+        if not row:
+            return None
+        else:
+            typeID,groupID,typeName,description,graphicID,radius,mass,volume,capacity,portionSize,raceID,basePrice,published,marketGroupID,chanceOfDuplicating,iconID = row
+            return {
+                "typeID" : int(typeID),
+                "groupID" : int(groupID),
+                "typeName" : typeName,
+                "description" : description,
+                "radius" : float(radius),
+                "mass" : float(mass),
+                "volume" : float(volume),
+                "capacity" : float(capacity),
+                "portionSize" : int(portionSize),
+                "raceID" : int(raceID),
+                "basePrice" : float(basePrice),
+                "marketGroupID" : int(marketGroupID),
+                "chanceOfDuplicating" : float(chanceOfDuplicating),   
+            }
+
+    def getTypeEffectsByID(self, typeID):
+        self.cursor.execute("""
+                            SELECT typeID, effectID, isDefault
+                            FROM dgmTypeEffects
+                            WHERE typeID='%s'
+                            """ % typeID)
+        results = []
+        while True:
+            try:
+                row = self.cursor.next()
+            except StopIteration:
+                break
+            else:
+                if row[2] == 1:
+                    isDefault = True
+                else:
+                    isDefault = False
+                results += [{
+                    "typeID" : int(row[0]),
+                    "effectID" : int(row[1]),
+                    "isDefault" : isDefault
+                }]
+                
+        return results
+            
+        
+    def getEffectByID(self, effectID):
+        self.cursor.execute("""
+                            SELECT *
+                            FROM dgmEffects
+                            WHERE effectID='%s'
+                            """ % effectID)
+        while True:
+            try:
+                row = self.cursor.next()
+            except StopIteration:
+                break
+            else:
+                return {
+                    "effectID" : row[0],
+                    "effectName" : row[1],
+                    "effectCategory" : row[2],
+                    "description" : row[5],
+                    "isOffensive" : row[7],
+                    "isAssistance" : row[8],
+                    "durationAttribueID" : row[9],
+                    "trackingSpeedAttributeID" : row[10],
+                    "dischargeAttributeID" : row[11],
+                    "rangeAttributeID" : row[12],
+                    "falloffAttributeID" : row[13],
+                    "displayName" : row[16],
+                    "isWarpSafe" : row[17],
+                    "rangeChance" : row[18],
+                    "electronicChance" : row[19],
+                    "propulsionChance" : row[20],
+                    "npcUsageChanceAttributeID" : row[23],
+                    "npcActivationChanceAttributeID" : row[24],
+                    "fittingUsageChanceAttributeID" : row[25]
+                }
+    def getTypeAttributesByID(self, typeID):
+        self.cursor.execute()
+            #typeID ->
+            #    dgmTypeEffects : typeID, effectID, isDefault
+            #        effectID ->
+            #            dgmEffects : effectID, effectName, effectCategory, description, isOffensive, isAssistance, durationAttributeID, trackingSpeedAttributeID, dischargeAttributeID, rangeAttributeID, falloffAttributeID, disallowAutoRepeat, displayName, isWarpSafe, rangeChance, electronicChance, propulsionChance, npcUsageChangeAttributeID, npcActivationChanceAttributeID, fittingUsageChanceAttributeID
+            #            all .*?AttributeIDs feed into dgmAttributeTypes
+            #    dgmTypeAttributes : typeID, attributeID, valueInt, valueFloat
+            #        attributeID ->
+            #            dgmAttributeTypes : attributeID, attributeName, defaultValue, displayName, unitID, stackable, highIsGood, categoryID
+            #                unitID ->
+            #                    eveUnits : unitID, unitName, displayName, description
+            #                categoryID ->
+            #                    dgmAttributeCategories : categoryID, categoryName, categoryDescription
+            #    invMetaTypes : typeID, parentTypeID, metaGroupID
+            #        parentTypeID -> feeds back into invTypes
+            #        metaGroupID ->
+            #           invMetaGroups : metaGroupID, metaGroupName, description
+            #            
+            #groupID ->
+            #    invGroups : groupID, groupName, categoryID, description, useBasePrice, allowManufacture, allowRecycler, anchored, anchorable, fittableNonSingleton
+            #        categoryID ->
+            #            invCategories : categoryID, categoryName, description
+            #raceID ->
+            #    chrRaces : raceID, raceName, description, shortDescription
+            #marketGroupID ->
+            #    invMarketGroups : marketGroupID, parentGroupID, marketGroupName, hasTypes
+            #    parentGroupID feeds back
+            
+            
+        #
+#typeID|groupID|typeName|description|graphicID|radius|mass|volume|capacity|portionSize|raceID|basePrice|published|marketGroupID|chanceOfDuplicating|iconID
     def getItemNameByID(self, ID):
         self.cursor.execute("""SELECT typeID,typeName
                             FROM invTypes
