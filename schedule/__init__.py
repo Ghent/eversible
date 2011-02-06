ce55552fcadc67b5ef08f76a42eba04c231146d3
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-02-06 03:05:50 +0000

    Removed debug logging -> mail notification seems to be working

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -14,16 +14,6 @@ import os
 import calendar
 import re
 from misc import functions
-class LOGGER:
-    def __init__(self):
-        self.LOGFILE = open("mail.log","a")
-    def write(self, message):
-        self.LOGFILE.write(message + "\n")
-        self.LOGFILE.flush()
-        #print "(%s) %s" % (time.asctime(), message)
-    def shutdown(self):
-        self.LOGFILE.flush()
-        self.LOGFILE.close()
         
 class Scheduler:
     def __init__(self):
@@ -33,9 +23,7 @@ class Scheduler:
         
         self.MAIL_RECORD = {}
         #char ID : {messageID: last mailID, sentTime : sent time}
-        
-        self.LOG = LOGGER()
-        
+                
     def start(self, refreshtime=300, connection=None):
         while True:
             self.QUEUE.insert(self.checkAPIurls, None, time.time())
@@ -51,35 +39,22 @@ class Scheduler:
             API = None
             mailXML = None
             nick = hostname.split("!")[0]
-            self.LOG.write("%s: Currently checking mail for %s (hostname: %s)" % (time.asctime().split()[3], nick, hostname))
             API = self.USERS.retrieveUserByHostname(hostname)["apiObject"]
-            self.LOG.write("> Got API object")
             requesturl = os.path.join(API.API_URL, "char/MailMessages.xml.aspx")
             postdata = {
                 "apiKey" : API.API_KEY,
                 "userID" : API.USER_ID,
                 "characterID" : API.CHAR_ID,
             }
-            self.LOG.write("> Char ID: %s" % API.CHAR_ID)
-            self.LOG.write("> Getting mail XML")
-            self.LOG.write(">> Querying CACHE")
             mailXML = self.CACHE.requestXML(requesturl, postdata, deleteOld=False)
             if not mailXML:
-                self.LOG.write(">> CACHE failed")
-                self.LOG.write(">> Querying API")
                 mailXML = urllib2.urlopen(requesturl, urllib.urlencode(postdata)).read()
-                self.LOG.write(">> Inserting XML into CACHE")
-                self.CACHE.insertXML(requesturl, "char", mailXML, API._getCachedUntil(mailXML), postdata)
-            self.LOG.write(">> Got XML (%i lines)" % len(mailXML.split("\n")))
             
             
-            self.LOG.write("> Checking if last mail recorded in memory")
             if API.CHAR_ID in self.MAIL_RECORD.keys():
                 latest_time = self.MAIL_RECORD[API.CHAR_ID]["sentTime"]
                 latest_id = self.MAIL_RECORD[API.CHAR_ID]["messageID"]
-                self.LOG.write(">> Recorded! latest_time: %s, latest_id: %s" % (latest_time, latest_id))
             else:
-                self.LOG.write(">> Not recorded! Checking user database")
                 result = self.USERS.getMessageID(API.CHAR_ID)
                 if result:
                     self.MAIL_RECORD[API.CHAR_ID] = {}
@@ -87,9 +62,7 @@ class Scheduler:
                     self.MAIL_RECORD[API.CHAR_ID]["messageID"] = result[0]
                     latest_time = result[1]
                     latest_id = result[0]
-                    self.LOG.write(">>> Stored in database! latest_time: %s, latest_id: %s" % (latest_time, latest_id))
                 else:
-                    self.LOG.write(">>> No record for this char, setting to None")
                     latest_time = None
                     latest_id = None                
                 
@@ -97,8 +70,6 @@ class Scheduler:
             new_latest_id = None
             
             newIDs = []
-            self.LOG.write("> Parsing XML")
-            self.LOG.write(">> Cached Until: %s" % time.asctime(time.gmtime(API._getCachedUntil(mailXML))).split()[3])
             rows = re.finditer("\<row messageID=\"(?P<messageID>\d+)\" senderID=\"(?P<senderID>\d+)\" sentDate=\"(?P<sentDate>\d+-\d+-\d+ \d+:\d+:\d+)\" title=\"(?P<title>.*?)\" toCorpOrAllianceID=\"(?P<toCorpOrAllianceID>.*?)\" toCharacterIDs=\"(?P<toCharacterIDs>.*?)\" toListID=\"(?P<toListID>.*?)\" \/\>", mailXML)
             #cycle through
             count = 0
@@ -111,7 +82,6 @@ class Scheduler:
                     messageID = int(row["messageID"])
                     sentDate = calendar.timegm(time.strptime(row["sentDate"], "%Y-%m-%d %H:%M:%S"))
                     if count < 5:
-                        self.LOG.write("> Mail id: %s, sentDate: %s, timediff: %s" % (messageID, sentDate, latest_time - sentDate))
                         count += 1
                     
                     if sentDate > latest_time:
@@ -119,9 +89,7 @@ class Scheduler:
                             new_latest_time = sentDate
                             new_latest_id = messageID
                         newIDs += [messageID]
-                        self.LOG.write("> New mail! sentDate: %s, messageID: %s" % (sentDate, messageID))
             if newIDs:
-                self.LOG.write("> There is definitely new mail -> reporting it")
                 newIDs_len = len(newIDs)
                 connection.notice(nick, functions.parseIRCBBCode("You have [colour=red]%i[/colour] new mails" % newIDs_len))
                 
@@ -140,16 +108,11 @@ class Scheduler:
                     connection.notice(nick, functions.parseIRCBBCode("First %i ids are: [b]%s[/b]" % (count, "[/b], [b]".join(ids))))
                     
                 #insert new data into users
-                self.LOG.write("> Inserting new data into user database")
                 self.USERS.insertMessageID(API.CHAR_ID, new_latest_id, new_latest_time)
-                self.LOG.write("> Adding new data to memory")
                 self.MAIL_RECORD[API.CHAR_ID] = {
                     "messageID" : new_latest_id,
                     "sentTime" : new_latest_time
                 }
-            else:
-                self.LOG.write("> No new mail")
-            self.LOG.write(">>>>>> DONE <<<<<<<")
         
     def checkAPIurls(self):
         API = api.API()
@@ -190,6 +153,7 @@ class _QUEUE:
             handler must be a function that takes a single argument of the returned data from the inputed function
             
             e.g.
+            >>> import time
             >>> def func(input):
             ...     print "func called!"
             ...     return input
@@ -199,7 +163,7 @@ class _QUEUE:
             ...     print returned
                 
             >>> s = QUEUE()
-            >>> schedID = s.insert(func, handler, "Hello World!")
+            >>> schedID = s.insert(func, handler, time.time(), "Hello World!")
             >>> s.run()
             func called!
             handler called!
