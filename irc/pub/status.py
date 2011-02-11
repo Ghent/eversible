2c5711a33f3d1d2cb0d54d3c4c1a03afce2ab9f8
Ghent <ghentlinux@gmail.com>
2011-02-11 09:49:48 -0600

    Removed flavor text and added locale formatting

--- irc/pub/status.py
+++ irc/pub/status.py
@@ -10,7 +10,7 @@ import api
 API = api.API()
 
 def index(connection, event, config):
-    #locale.setlocale(locale.LC_ALL, "en_US")
+    locale.setlocale(locale.LC_ALL, config["core"]["locale"])
     serverstatus = API.Server("status")
     status = serverstatus["status"]
     online = serverstatus["online"]
@@ -18,15 +18,12 @@ def index(connection, event, config):
     if status == "Online":
         if online >= 50000:
             players = "\x035 " + locale.format("%d", online, True) + "\x03"
-            players = players + " \x0314(Shooting fish in a barrel...)\x03"
         elif online >= 30000:
             players = "\x037 " + locale.format("%d", online, True) + "\x03"
-            players = players + " \x0314(It's getting a little crowded...)\x03"
         else:
             players = "\x033 " + locale.format("%d", online, True) + "\x03"
-            players = players + " \x0314(Space is empty...)\x03"
 
-        message = "\x02Server\x02: \x0311Tranquility\x03  \x02Status\x02: \x039Online\x03  \x02Players\x02:%s \x02Server Time (GMT)\x02: %s" % (players, servertime)
+        message = "\x02Server\x02: \x0311Tranquility\x03  \x02Status\x02: \x039Online\x03  \x02Players\x02:%s  \x02Server Time (GMT)\x02: %s" % (players, servertime)
 
     elif status == "Offline":
         message = "\x02Server\x02: \x0310Tranquility\x03  \x02Status\x02: \x034Offline\x03"
