f74b4b799bc08ff2d9ce8828e5e2e8fd03ce5466
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-02-14 14:28:39 +0000

    Testing 'clever' parsing

--- irc/pub/pve.py
+++ irc/pub/pve.py
@@ -9,11 +9,20 @@ import time
 import users
 import api
 
+def parseParsable(parsable):
+    if " AND " in parsable:
+        systems = []
+        for pot_sys in parsable.split(" AND "):
+            systems += [pot_sys.replace(" IN ","").strip()]
+    elif "," in parsable:
+        systems = []
+        for pot_sys in parsable.split(","):
+            systems += [pot_sys.strip()]
+    else:
+        systems = [parsable]
+    return systems
 
 def index(connection, event, config):
-    #no args
-    # perhaps system name in the future
-
     locale.setlocale(locale.LC_ALL, config["core"]["locale"])
     sourceHostName = event.source()
     sourceNick = event.source().split("!")[0]
@@ -28,6 +37,114 @@ def index(connection, event, config):
         connection.privmsg(event.target(), "User \x034\x02'%s'\x03\x02 not recognised" % sourceNick)
     else:
         API = APItest["apiObject"]
+        
+        #parse inputs
+        try:
+            inputs = " " + event.arguments()[0].split(" ", 1)[1]
+        except IndexError:
+            since = 0
+            to = 2147483647
+            systems = [None]
+        else:
+            #syntax:
+            #     from DD/MM to DD/MM
+            #     from DD/MM
+            #     since DD/MM to DD/MM
+            #     since/from DDst/nd/th (of Month/Mth) to DDst/nd/th (of Month/mth)
+            #     DD/MM
+            #     DDst/nd/th (of Month/Mth)
+            #     DD/MM - DD/MM
+            #     DDst/nd/th (of Month/Mth) - DDst/nd/th (of Month/Mth)
+            
+            if " FROM " in inputs or " SINCE " in inputs:
+                if " FROM " in inputs:
+                    separator = " FROM "
+                else:
+                    separator = " SINCE "
+                #possibilities:
+                # just from
+                #1   from x
+                # from and to
+                #2   from x to x
+                #3   to x from x
+                # from and in
+                #4   from x in x (and in x ...)
+                #5   in x (and in x ...) from x
+                # from and to and in
+                #6   from x in x (and in x ...) to x
+                #7   from x to x in x (and in x ...)
+                #8   to x from x in x (and in x ...)
+                #9   to x in x (and in x ...) from x
+                #10  in x (and in x ...) to x from x
+                #11  in x (and in x ...) from x to x
+                if " TO " not in inputs and " IN " not in inputs:
+                    #option 1
+                    since = inputs.split(separator)[1].strip()
+                    to = 2147483647
+                    systems = [None]
+                elif " TO " in inputs and " IN " not in inputs:
+                    if inputs.index(separator) < inputs.index(" TO "):
+                        #option 2
+                        since = inputs.split(separator)[1].split(" TO ")[0].strip()
+                        to = inputs.split(" TO ")[1].strip()
+                        systems = [None]
+                    else:
+                        #option 3
+                        since = inputs.split(separator)[1].strip()
+                        to = inputs.split(" TO ")[1].split(separator)[0].strip()
+                        systems = [None]
+                elif " TO " not in inputs and " IN " in inputs:
+                    if inputs.index(separator) < inputs.index(" IN "):
+                        #option 4
+                        systems = parseParsable(inputs.split(" IN ", 1)[1].strip())
+                        since = inputs.split(separator)[1].split(" IN ")[0].strip()
+                        to = 2147483647
+                    else:
+                        #option 5
+                        systems = parseParsable(inputs.split(" IN ", 1)[1].split(separator)[0].strip())
+                        since = inputs.split(separator)[1].strip()
+                        to = 2147483647
+                elif " TO " in inputs and " IN " in inputs:
+                    #options 6-11
+                    if inputs.index(separator) > inputs.index(" IN ") and inputs.index(" IN ") > inputs.index(" TO "):
+                        #6   from x in x (and in x ...) to x
+                        since = inputs.split(separator)[1].split(" IN ")[0].strip()
+                        to = inputs.split(" TO ")[1].strip()
+                        systems = parseParsable(inputs.split(" IN ",1)[1].split(" TO ")[0].strip())
+                    elif inputs.index(separator) > inputs.index(" TO ") and inputs.index(" TO ") > inputs.index(" IN "):
+                        #7   from x to x in x (and in x ...)
+                        since = inputs.split(separator)[1].split(" TO ")[0].strip()
+                        to = inputs.split(" TO ")[1].split(" IN ")[0].strip()
+                        systems = parseParsable(inputs.split(" IN ", 1)[1])
+                    elif inputs.index(" TO ") > inputs.index(separator) and inputs.index(separator) > inputs.index(" IN "):
+                        #8   to x from x in x (and in x ...)
+                        since = inputs.split(separator)[1].split(" IN ")[0].strip()
+                        to = inputs.split(" TO ")[1].split(separator)[0].strip()
+                        systems = parseParsable(inputs.split(" IN ", 1)[1].strip())
+                    elif inputs.index(" TO ") > inputs.index(" IN ") and inputs.index(" IN ") > inputs.index(separator):
+                        #9   to x in x (and in x ...) from x
+                        since = inputs.split(separator)[1].strip()
+                        to = inputs.split(" TO ")[1].split(" IN ")[0].strip()
+                        systems = parseParsable(inputs.split(" IN ", 1)[1].split(" FROM ")[0].strip())
+                    elif inputs.index(" IN ") > inputs.index(" TO ") and inputs.index(" TO ") > inputs.index(separator):
+                        #10  in x (and in x ...) to x from x
+                        since = inputs.split(separator)[1].strip()
+                        to = inputs.split(" TO ")[1].split(separator)[0].strip()
+                        systems = parseParsable(inputs.split(" IN ", 1)[1].split(" TO ")[0].strip())
+                    else:
+                        #11  in x (and in x ...) from x to x
+                        since = inputs.split(separator)[1].split(" TO ")[0].strip()
+                        to = inputs.split(" TO ")[1].strip()
+                        systems = parseParsable(inputs.split(" IN ", 1)[1].split(" FROM ")[0].strip())
+                        
+                        
+            elif "since" in inputs:
+                if "to" in inputs:
+                    since = inputs.split("since")[1].split("to")[0].strip()
+            
+            print "since:%s, to:%s, systems:%r" % (since, to, systems)
+                
+        
         try:
             walletdict = API.Char("wallet")
         except api.APIError:
