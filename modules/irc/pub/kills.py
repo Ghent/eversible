1051a3aa9acbedc18a11a6630fa336d5fd6a7b71
mountainpenguin [seatiger] <pinguino.de.montana@googlemail.com>
2012-01-12 13:20:05 +0000

    Fixed bug -> the %i formatting operator requires an integer, a string was being passed

--- modules/irc/pub/kills.py
+++ modules/irc/pub/kills.py
@@ -58,7 +58,7 @@ def index(connection, event, config):
             constellationName = systemInfo["constellationName"]
 
             connection.privmsg(event.target(), "\x02System\x02:  \x02%s\x02/\x0310%s\x03/\x032%s\x03 (%s)" % (solarSystemName, constellationName, regionName, sec))
-            connection.privmsg(event.target(), "\x02Ships\x02:   %i" % locale.format("%d", shipKills, True))
-            connection.privmsg(event.target(), "\x02Pods\x02:    %i" % locale.format("%d", podKills, True))
-            connection.privmsg(event.target(), "\x02NPCs\x02:    %i" % locale.format("%d", npcKills, True))
+            connection.privmsg(event.target(), "\x02Ships\x02:   %s" % locale.format("%d", shipKills, True))
+            connection.privmsg(event.target(), "\x02Pods\x02:    %s" % locale.format("%d", podKills, True))
+            connection.privmsg(event.target(), "\x02NPCs\x02:    %s" % locale.format("%d", npcKills, True))
             #connection.privmsg(event.target(), "In the last hour, there were %i ship kills, %i pod kills and %i NPC kills in system %s" % (shipKills, podKills, npcKills, solarSystemName))
